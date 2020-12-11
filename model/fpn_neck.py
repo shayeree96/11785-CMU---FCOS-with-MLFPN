595006358004a04bccf45bf82d63aff59a15c67f
dhruvv301292 <dhruvv301292@gmail.com>
2020-12-10 20:10:21 -0500

    changed p5 condition

--- model/fpn_neck.py
+++ model/fpn_neck.py
@@ -4,7 +4,6 @@ import math
 
 
 class FPN(nn.Module):
-    '''only for resnet50,101,152'''
     def __init__(self,features=256,use_p5=True):
         super(FPN,self).__init__()
         self.prj_5 = nn.Conv2d(2048, features, kernel_size=1)
@@ -13,10 +12,7 @@ class FPN(nn.Module):
         self.conv_5 =nn.Conv2d(features, features, kernel_size=3, padding=1)
         self.conv_4 =nn.Conv2d(features, features, kernel_size=3, padding=1)
         self.conv_3 =nn.Conv2d(features, features, kernel_size=3, padding=1)
-        if use_p5:
-            self.conv_out6 = nn.Conv2d(features, features, kernel_size=3, padding=1, stride=2)
-        else:
-            self.conv_out6 = nn.Conv2d(2048, features, kernel_size=3, padding=1, stride=2)
+        self.conv_out6 = nn.Conv2d(features, features, kernel_size=3, padding=1, stride=2)
         self.conv_out7 = nn.Conv2d(features, features, kernel_size=3, padding=1, stride=2)
         self.use_p5=use_p5
         self.apply(self.init_conv_kaiming)
